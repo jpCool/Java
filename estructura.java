84d8a2795f042f3d4f4b8febe8b81cbff728a4d7
Jean Pierre Nureña Coronel <pelotero_357@hotmail.com>
2014-12-24 21:45:29 -0500

    Update estructura.java

--- estructura.java
+++ estructura.java
@@ -4,7 +4,7 @@ public class AprendiendoJava{
 
 	public static void main(String[] args){
 
-		//Imprimimos por consola el mensaje : Hola Mundo, Estoy aprendiendo JAVA
+		//Imprimimos por consola el Mensaje : Hola Mundo, Estoy aprendiendo JAVA
 		
 			System.out.println("Hola Mundo, Estoy aprendiendo JAVA");
 
