4079e470a7941feb7b6fee551cee3cf20df1ad42
jeanpierre coronel <pelotero_357@hotmail.com>
2014-12-24 21:44:27 -0500

    estructura basica

---
+++ estructura.java
@@ -0,0 +1,14 @@
+public class AprendiendoJava{
+
+	//Crear nuestra clase principal
+
+	public static void main(String[] args){
+
+		//Imprimimos por consola el mensaje : Hola Mundo, Estoy aprendiendo JAVA
+		
+			System.out.println("Hola Mundo, Estoy aprendiendo JAVA");
+
+
+	}
+
+}
